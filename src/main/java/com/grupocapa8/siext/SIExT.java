851aafdf5f11e81786c321892e587234160ea382
MatBustamant <matexequiel.2012@gmail.com>
2025-08-26 12:00:13 -0300

    Estado inicial de proyecto

---
+++ src/main/java/com/grupocapa8/siext/SIExT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.grupocapa8.siext;
+
+/**
+ *
+ * @author Matias
+ */
+public class SIExT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
