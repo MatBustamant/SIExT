e6beb7f2971ade20c8ccc91ec3efa9f1c64eaf1b
MatBustamant <matexequiel.2012@gmail.com>
2025-09-07 20:29:57 -0300

    jersey y grizzly implementado con test inicial

---
+++ src/main/java/com/grupocapa8/siext/controladorrest/TestResource.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.grupocapa8.siext.controladorrest;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+
+/**
+ *
+ * @author Matias
+ */
+@Path("testresource")
+public class TestResource {
+
+    public static final String CLICHED_MESSAGE = "Hello World!";
+
+    @GET
+    @Produces("text/plain")
+    public String getHello() {
+        return CLICHED_MESSAGE;
+    }
+
+}
